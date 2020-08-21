8586575c158d8a964b6764d0432299b4c4f44de3
Tom McIntosh <tom_mcintosh@outlook.com>
2020-08-22 00:26:36 +1000

    Started writing Youtube Class

---
+++ src/mediaTypes/mkYoutube.js
@@ -0,0 +1,62 @@
+import {message} from "../message";
+import {store} from "../store";
+import {pauseAllExcept} from "../controls";
+const YTPlayer = require('yt-player')
+
+export class mkVimeo {
+
+    constructor(item) {
+
+        this.item = item;
+
+        // Find this element in the DOM
+        let domReference = this.find()
+
+        if (!domReference) {
+            message.error.badSelector(this.item.selector, 'mkAudio.constructor()', 'Is this a valid query selector?')
+        }
+
+        this.domRef = domReference;
+        this.player = new YTPlayer(this.item.selector)
+
+        this.player.on('play', () => {
+            this.onPlay()
+        })
+
+        this.player.on('pause', () => {
+            this.onPause()
+        })
+
+        this.player.on('ended', () => {
+            this.onStop()
+        })
+
+    }
+
+    find() {
+        return document.querySelector(this.item.selector)
+    }
+
+    play() {
+        this.player.play()
+    }
+
+    onPlay() {
+        if (store.config.playExclusive) {
+            pauseAllExcept(this.item.name)
+        }
+    }
+
+    pause() {
+        this.player.pause()
+    }
+
+    onPause() {}
+
+    stop() {
+        this.player.pause()
+    }
+
+    onStop() {}
+
+}
